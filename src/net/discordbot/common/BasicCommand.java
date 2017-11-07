fffff4409da91cfacaffc05f6120454501699c76
Mihai <mihai.andreescu995@gmail.com>
2017-11-07 00:31:46 -0600

    Removed empty class Utils.

--- src/net/discordbot/common/BasicCommand.java
+++ src/net/discordbot/common/BasicCommand.java
@@ -7,11 +7,11 @@ import java.lang.annotation.RetentionPolicy;
  * Marks instance methods that represent commands for the DiscordBot. The string parameter should
  * describe its behavior. The format of the command should be as follows:
  *
- * @BasicCommand("describe the command here")
+ * \@BasicCommand("describe the command here")
  * public void [commandName](Message messageThatTriggeredTheCommand, other args...) {...}
  *
- * At the moment, the only argument types that are allowed are int, double and string. To change
- * this, please modify Utils.covert().
+ * At the moment, the only argument types that are allowed are ints, doubles and strings. To change
+ * this, please modify `CommandInvoker.CONVERTER`.
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface BasicCommand {
