4de0a3f87fa617438048d4397b5b375cf200ac2f
Mihai <mihai.andreescu995@gmail.com>
2017-11-18 16:54:18 -0600

    Improved ConversionBot

--- src/net/discordbot/bots/ConversionBot.java
+++ src/net/discordbot/bots/ConversionBot.java
@@ -1,143 +1,131 @@
 package net.discordbot.bots;
 
+import com.google.common.collect.ImmutableMultimap;
+import net.discordbot.common.BasicCommand;
 import net.discordbot.common.DiscordBot;
 import net.discordbot.common.TextListener;
 import net.discordbot.util.Config;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.User;
+import org.jscience.physics.amount.Amount;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import javax.measure.Measure;
-import javax.measure.converter.UnitConverter;
+import javax.measure.unit.Unit;
+import javax.measure.unit.UnitFormat;
 
 import static javax.measure.unit.NonSI.*;
 import static javax.measure.unit.SI.*;
 
 public class ConversionBot extends DiscordBot implements TextListener {
 
-  private static final Pattern IS_UNIT = Pattern.compile("(-?[0-9]*[.,]?[0-9]+)\\s?([a-zA-Z]{1,})");
-  // Length units
-  private static final UnitConverter fromInch = INCH.getConverterTo(CENTIMETER); // in
-  private static final UnitConverter fromFoot = FOOT.getConverterTo(CENTIMETER); // ft
-  private static final UnitConverter fromYard = YARD.getConverterTo(CENTIMETER); // yd
-  private static final UnitConverter fromMile = MILE.getConverterTo(CENTIMETER); // mi
-  //Mass units
-  private static final UnitConverter fromOunce = OUNCE.getConverterTo(GRAM); // oz
-  private static final UnitConverter fromPound = POUND.getConverterTo(GRAM); /// lb
-  //Volume units
-  private static final UnitConverter fromGallon = GALLON_UK.getConverterTo(LITER); //ga
-  //Temperature units
-  private static final UnitConverter fromFarenheit = FAHRENHEIT.getConverterTo(CELSIUS); //fa
-  private User SatanName;
+  private static final String NUMBER = "-?[0-9]*[.,]?[0-9]+";
+
+  private static final String UNIT = "[a-zA-Z]*[.a-zA-Z]?[a-zA-Z]+";
+
+  private static final Pattern IS_UNIT =
+      Pattern.compile(String.format("(%s)\\s*(%s)", NUMBER, UNIT));
+
+  private static final String CONVERSION_FORMAT = "FYI %s is %.2f%s in non-retarded units";
+
+  private User satanName;
+
+  private static final ImmutableMultimap<Unit, Unit> UNIT_PREFERENCES =
+      ImmutableMultimap.<Unit, Unit>builder()
+          .putAll(KILOGRAM, KILOGRAM, GRAM, METRIC_TON, MICRO(GRAM), NANO(GRAM))
+          .putAll(METER, METER, CENTIMETER, KILOMETER)
+          .putAll(CUBIC_METRE, LITER, MILLI(LITER))
+          .put(KELVIN, CELSIUS)
+          .build();
 
   @Override
   public void prepare(JDA jda, Config cfg) {
     super.prepare(jda, cfg);
-    SatanName = jda.getSelfUser();
+    satanName = jda.getSelfUser();
   }
 
   @Override
   public boolean parseMessage(Message message) {
-    // TODO: use less code
-    if (message.getAuthor() == SatanName)
+    if (message.getAuthor() == satanName) {
       return false;
-    String result;
-    boolean replySent = false;
-    Matcher matches = IS_UNIT.matcher(message.getContent());
-    while(matches.find()) {
-      double x = Double.parseDouble(matches.group(1).replace(',','.'));
-      if(matches.group(2).length() == 1)
-        result = buildResponse(x,matches.group(2).substring(0,1));
-      else
-        result = buildResponse(x,matches.group(2).substring(0,2));
-      if (result != null) {
-        reply(message, "%s in non-retarded units" ,result).now();
-        replySent = true;
+    }
+    Matcher matcher = IS_UNIT.matcher(message.getContent());
+    boolean processed = false;
+    while (matcher.find()) {
+      processed |= convert(message, matcher.group(0));
+    }
+    return processed;
+  }
+
+  @BasicCommand("converts imperial units to metric units")
+  public boolean convert(Message msg, String quantity) {
+    quantity = quantity.replace(',', '.');
+    Amount amount;
+    try {
+      amount = Amount.valueOf(quantity);
+    } catch (IllegalArgumentException e) {
+      return false;
+    }
+    Unit oldUnit = amount.getUnit();
+
+    Unit unit = amount.getUnit().getStandardUnit();
+    Collection<Unit> possibleUnits = UNIT_PREFERENCES.get(unit);
+
+    if (unit.equals(oldUnit) || possibleUnits.contains(oldUnit)) {
+      // Unit is already one of the preferred ones or there are no defined preferences. Do nothing.
+      return false;
+    }
+    if (possibleUnits.isEmpty()) {
+      possibleUnits = List.of(unit);
+    }
+
+    amount = getPreferredAmount(amount, possibleUnits);
+    reply(msg, CONVERSION_FORMAT, quantity, amount.getEstimatedValue(), amount.getUnit()).soon();
+    return true;
+  }
+
+  /** Converts the `amount` to the best suited unit in `possibleUnits`. */
+  private static Amount getPreferredAmount(Amount amount, Collection<Unit> possibleUnits) {
+    double best = Double.POSITIVE_INFINITY;
+    for (Unit unit : possibleUnits) {
+      Amount option = amount.to(unit);
+      double current = score(option);
+      if (current < best) {
+        amount = option;
+        best = current;
       }
     }
-    return replySent;
+    return amount;
   }
 
-  private String buildResponse(double number, String unit){
-    double x;
-    String s = "FYI " + String.valueOf(number) + " " +  unit + " is ";
-    if (number <= 0 && (unit != "fa" || unit != "F"))
-      return null;
-    switch(unit){
-      case "in":
-        x = fromInch.convert(Measure.valueOf(number, INCH).doubleValue(INCH));
-        return s + upLength(x);
-      case "ft":
-        x = fromFoot.convert(Measure.valueOf(number, FOOT).doubleValue(FOOT));
-        return s + upLength(x);
-      case "yd":
-        x = fromYard.convert(Measure.valueOf(number, YARD).doubleValue(YARD));
-        return s + upLength(x);
-      case "mi":
-        x = fromMile.convert(Measure.valueOf(number, MILE).doubleValue(MILE));
-        return s + upLength(x);
-      case "oz":
-        x = fromOunce.convert(Measure.valueOf(number, OUNCE).doubleValue(OUNCE));
-        return s + upMass(x);
-      case "lb":
-        x = fromPound.convert(Measure.valueOf(number, POUND).doubleValue(POUND));
-        return s + upMass(x);
-      case "ga":
-        x = fromGallon.convert(Measure.valueOf(number, GALLON_UK).doubleValue(GALLON_UK));
-        return s + upVolume(x);
-      case "F":
-      case "fa":
-        x = fromFarenheit.convert(Measure.valueOf(number, FAHRENHEIT).doubleValue(FAHRENHEIT));
-        return s + upTemperature(x);
-    }
-    return null;
+  /**
+   * Returns a score that indicates how well the `amount` is characterized by its unit of measure.
+   * The lower the score is the stronger the preference.
+   */
+  private static double score(Amount amount) {
+    return Math.abs(1 - Math.log10(1e-20 + Math.abs(amount.getEstimatedValue())));
   }
 
-  private String upMass (double x){
-    if (x >= 1000) {
-      x = Math.round(x / 10);
-      x = x/100;
-      return String.valueOf(x) + " kg";
-    }
-    else{
-      x = Math.round(x * 100);
-      x = x/100;
-      return String.valueOf(x) + " g";
-    }
+  static {
+    alias(INCH, "in", "inch", "inches");
+    alias(FOOT, "ft", "foot", "feet");
+    alias(YARD, "yd", "yard", "yards");
+    alias(MILE, "mi", "mile", "miles", "mila");
+    alias(OUNCE, "oz", "ounce", "ounces", "uncie", "uncii");
+    alias(OUNCE_LIQUID_US, "fl.oz", "floz");
+    alias(POUND, "lb", "lbs", "pound", "pounds");
+    alias(FAHRENHEIT, "F", "Fa", "Fahrenheit");
+    alias(GALLON_LIQUID_US, "ga", "gallon", "gallons");
+    alias(GALLON_LIQUID_US.divide(8), "pint", "pints");
   }
 
-  private String upVolume (double x){
-    x = Math.round(x * 100);
-    x = x/100;
-    return String.valueOf(x) + " l";
-  }
-
-  private String upLength (double x){
-    if (x >= 100000){
-      x = Math.round(x / 1000);
-      x = x/100;
-      return String.valueOf(x) + " km";
-    }
-    else
-    if (x >= 100){
-      x = Math.round(x);
-      x = x/100;
-      return String.valueOf(x) + " m";
-    }
-    else{
-      x = Math.round(x * 100);
-      x = x/100;
-      return String.valueOf(x) + " cm";
+  private static void alias(Unit unit, String... aliases) {
+    UnitFormat units = UnitFormat.getInstance();
+    for (String alias : aliases) {
+      units.alias(unit, alias);
     }
   }
-
-  private String upTemperature (double x) {
-    x = Math.round(x * 100);
-    x = x/100;
-    return String.valueOf(x) + " C";
-  }
-
-
 }
